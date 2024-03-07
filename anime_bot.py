6abf5722626ded0ca4e8d9d96757f4d406fcd178
Sura <suri.murodova@gmail.com>
2024-03-07 16:18:11 +0300

    Add anime_bot.py

---
+++ anime_bot.py
@@ -0,0 +1,11 @@
+import requests
+
+
+api_url = 'http://api.open-notify.org/iss-now.json'
+
+response = requests.get(api_url)   # Отправляем GET-запрос и сохраняем ответ в переменной response
+
+if response.status_code == 200:    # Если код ответа на запрос - 200, то смотрим, что пришло в ответе
+    print(response.text)
+else:
+    print(response.status_code)
