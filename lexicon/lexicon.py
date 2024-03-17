06982c0c94a802dfede524a11dce594cb2086c9b
Sura <suri.murodova@gmail.com>
2024-03-17 18:51:59 +0300

    Add lexicon.py file.
    File consists bot answers dictionary.

---
+++ lexicon/lexicon.py
@@ -0,0 +1,8 @@
+LEXICON: dict[str, str] = {
+    '/start': "I'm here to help you choose an anime 🌟",
+    '/help': 'Contact the admin via the link https://t.me/Sura_1096 if you have any:'
+             '\n1. Questions ❓'
+             '\n2. Complaints 💢'
+             '\n3. Wishes 🔆',
+    'unknown': 'Sorry, but the logic of this bot does not include the command you sent 👀'
+}
