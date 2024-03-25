fe4e1acbb020ccc481d4476e395c84b2a96b31d1
Sura <suri.murodova@gmail.com>
2024-03-25 20:37:13 +0300

    Add two buttons to the keyboard

--- keyboards/keyboard_utils.py
+++ keyboards/keyboard_utils.py
@@ -6,9 +6,15 @@ from aiogram.utils.keyboard import ReplyKeyboardBuilder
 def genres_button():
     kb_builder = ReplyKeyboardBuilder()
 
-    buttons: list[KeyboardButton] = [
+    genre_buttons: list[KeyboardButton] = [
         KeyboardButton(text=f'{genre}') for genre in genres
     ]
 
-    kb_builder.row(*buttons, width=4)
+    option_buttons: list[KeyboardButton] = [
+        KeyboardButton(text='Apply filter 🟢'),
+        KeyboardButton(text='To home page ⛩')
+    ]
+
+    kb_builder.row(*genre_buttons, width=4)
+    kb_builder.row(*option_buttons, width=2)
     return kb_builder
