7675fa5f43ee804b26b3ccea8abdb5a57b195168
Sura <suri.murodova@gmail.com>
2024-03-25 19:47:45 +0300

    Add handler for Genre button that shows all anime genres

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -12,9 +12,17 @@ router = Router()
 @router.message(CommandStart())
 async def process_start_command(message: Message):
     await message.answer(text=LEXICON['/start'],
-                         reply_markup=start_menu())
+                         reply_markup=start_menu().as_markup(one_time_keyboard=True,
+                                                             resize_keyboard=True))
 
 
 @router.message(Command(commands=['help']))
 async def process_help_command(message: Message):
     await message.answer(text=LEXICON['/help'])
+
+
+@router.message(F.text == 'Genre 📁')
+async def process_genres_button(message: Message):
+    await message.answer(text='Here is all genres',
+                         reply_markup=genres_button().as_markup(one_time_keyboard=True,
+                                                                resize_keyboard=True))
