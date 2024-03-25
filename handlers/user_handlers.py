a2759d346068fc545c53d0bc51a96cb8935b6b7a
Sura <suri.murodova@gmail.com>
2024-03-25 20:51:04 +0300

    Change function in a reply_markup argument from set_menu() to user_choice()

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -12,8 +12,8 @@ router = Router()
 @router.message(CommandStart())
 async def process_start_command(message: Message):
     await message.answer(text=LEXICON['/start'],
-                         reply_markup=start_menu().as_markup(one_time_keyboard=True,
-                                                             resize_keyboard=True))
+                         reply_markup=user_choice().as_markup(one_time_keyboard=True,
+                                                              resize_keyboard=True))
 
 
 @router.message(Command(commands=['help']))
