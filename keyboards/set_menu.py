25e4d0e0277806d63c91645f9cb528c698cc7c2b
Sura <suri.murodova@gmail.com>
2024-03-25 21:13:36 +0300

    Add function for setting menu commands

---
+++ keyboards/set_menu.py
@@ -0,0 +1,13 @@
+from aiogram import Bot
+from aiogram.types import BotCommand
+from lexicon.lexicon_menu_commands import LEXICON
+
+
+async def set_main_menu(bot: Bot):
+    main_menu_commands = [
+        BotCommand(
+            command=command,
+            description=description
+        ) for command, description in LEXICON.items()
+    ]
+    await bot.set_my_commands(main_menu_commands)
