837185b3da70b486d57b074f256bdce10d37abe2
Sura <suri.murodova@gmail.com>
2024-04-18 16:05:02 +0300

    Rewrite process_genres_buttons function and use KeyboardPaginator class for buttons pagination

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,8 +1,10 @@
 from aiogram import F, Router
-from aiogram.types import Message, CallbackQuery
+from aiogram.types import Message, CallbackQuery, InlineKeyboardButton
 from aiogram.filters import Command, CommandStart
 from lexicon.lexicon import LEXICON
 from keyboards.keyboard_utils import user_choice_buttons, help_command_button, genres_buttons
+from aiogram_widgets.pagination import KeyboardPaginator
+from anime_genres.genres_list import genres
 
 
 router = Router()
@@ -21,6 +23,18 @@ async def process_help_command(message: Message):
 
 
 @router.callback_query(F.data == 'genres')
-async def process_genres_button(callback: CallbackQuery):
+async def process_genres_buttons(callback: CallbackQuery):
+    buttons: list[InlineKeyboardButton] = [
+        InlineKeyboardButton(text=f'{genres[genre_id]}', callback_data=str(genre_id)) for genre_id in genres
+    ]
+    buttons.append(InlineKeyboardButton(text='Apply filter 🟢', callback_data='apply filter'))
+    buttons.append(InlineKeyboardButton(text='To home page ⛩', callback_data='home page'))
+
+    paginator = KeyboardPaginator(
+        data=buttons,
+        router=router,
+        per_page=10,
+        per_row=(3, 3)
+    )
     await callback.message.answer(text='Here is all genres 🗂',
-                                  reply_markup=genres_buttons().as_markup())
+                                  reply_markup=paginator.as_markup())
