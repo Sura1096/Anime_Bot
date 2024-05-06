002e89e9fd87bc210449fc61738e4e87b215325d
Sura <suri.murodova@gmail.com>
2024-05-06 15:04:50 +0300

    Add temp handler for inline mode

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -1,9 +1,27 @@
-from aiogram import Router
-from aiogram.types import Message
+from aiogram import Router, F
+from aiogram.types import Message, InlineQuery, InlineQueryResultArticle, InputTextMessageContent
+import hashlib
 from lexicon.lexicon import (LEXICON)
+from Jikan_API.parse_data_from_api import ParseAnimeDataFromSearch
+from Jikan_API.API import JikanAPI
 
 
 router = Router()
+anime = JikanAPI()
+
+
+@router.inline_query(F.data)
+async def inline_echo(inline_query: InlineQuery):
+    text = inline_query.query or 'Echo'
+    input_content = InputTextMessageContent(message_text=text)
+    result_id = hashlib.md5(text.encode()).hexdigest()
+
+    item = [InlineQueryResultArticle(
+        id=result_id,
+        title='Text',
+        input_message_content=input_content
+    )]
+    await inline_query.answer(results=item, is_personal=True)
 
 
 @router.message()
