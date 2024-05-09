595fd77a9af3d35c1b6ba3133db1615548ab3d99
Sura <suri.murodova@gmail.com>
2024-05-09 15:04:14 +0300

    Delete inline query handler and comment handler for other messages

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -7,23 +7,8 @@ from Jikan_API.API import JikanAPI
 
 
 router = Router()
-anime = JikanAPI()
 
 
-@router.inline_query(F.data)
-async def inline_echo(inline_query: InlineQuery):
-    text = inline_query.query or 'Echo'
-    input_content = InputTextMessageContent(message_text=text)
-    result_id = hashlib.md5(text.encode()).hexdigest()
-
-    item = [InlineQueryResultArticle(
-        id=result_id,
-        title='Text',
-        input_message_content=input_content
-    )]
-    await inline_query.answer(results=item, is_personal=True)
-
-
-@router.message()
-async def rest_message_handler(message: Message):
-    await message.answer(text=LEXICON['unknown'])
+# @router.message()
+# async def rest_message_handler(message: Message):
+#     await message.answer(text=LEXICON['unknown'])
