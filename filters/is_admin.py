671c686087abcad1852602a55879253adaf6724c
Sura <suri.murodova@gmail.com>
2024-05-14 14:41:22 +0300

    Change text in my_filter

--- filters/is_admin.py
+++ filters/is_admin.py
@@ -2,6 +2,6 @@ from aiogram.types import Message
 
 
 def my_filter(message: Message):
-    if not message.text.startswith(':'):
+    if not message.text.startswith('Titles:'):
         return True
     return False
