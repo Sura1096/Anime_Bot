c70b8e7490126d1c08dd45562bba593a7ab326d5
Sura <suri.murodova@gmail.com>
2024-05-15 11:46:43 +0300

    Change a condition for other messages

--- filters/is_admin.py
+++ filters/is_admin.py
@@ -2,6 +2,6 @@ from aiogram.types import Message
 
 
 def my_filter(message: Message):
-    if not message.text.startswith('Titles:'):
+    if not message.photo:
         return True
     return False
