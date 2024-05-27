2ef3953f1aa1ba4a20946b43de24ded67673965d
Sura <suri.murodova@gmail.com>
2024-05-27 12:01:11 +0300

    Add docstring for my_filter

--- filters/is_admin.py
+++ filters/is_admin.py
@@ -2,6 +2,15 @@ from aiogram.types import Message
 
 
 def my_filter(message: Message):
+    """
+    Filter function for rest_message_handler.
+
+    Args:
+        message (Message): The message object from aiogram representing the incoming message.
+
+    Returns:
+        bool: True if the message does not start with '⛩ Titles:', otherwise False.
+    """
     if not message.text.startswith('⛩ Titles:'):
         return True
     return False
