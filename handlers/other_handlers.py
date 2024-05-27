2c1e2e91283052917f23b64086bf9ddd894e00a1
Sura <suri.murodova@gmail.com>
2024-05-27 12:05:10 +0300

    Add docstring for handler rest_message_handler

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -8,4 +8,10 @@ router = Router()
 
 @router.message(my_filter)
 async def rest_message_handler(message: Message):
+    """
+    Handles messages that do not match other specified handlers and deletes them.
+
+    Args:
+        message (Message): The message object from aiogram representing the incoming message.
+    """
     await message.delete()
