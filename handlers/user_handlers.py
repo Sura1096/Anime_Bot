0c9019b0a8abebbadbc10dbb3c7d817f2e0805c8
Sura <suri.murodova@gmail.com>
2024-05-28 13:54:24 +0300

    Add docstrings for each handler

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -10,18 +10,39 @@ router = Router()
 
 @router.message(CommandStart())
 async def process_start_command(message: Message):
+    """
+    Handles the /start command and sends a message with inline buttons for user options.
+
+    Args:
+        message (Message): The message object from aiogram representing the incoming /start command.
+    """
     await message.answer(text=LEXICON['/start'],
                          reply_markup=user_choice_buttons().as_markup())
 
 
 @router.message(Command(commands=['help']))
 async def process_help_command(message: Message):
+    """
+    Handles the /help command and responds with a message informing the user to contact the
+    admin for questions, complaints, or wishes, and includes an inline button that
+    facilitates contacting the admin.
+
+    Args:
+        message (Message): The message object from aiogram representing the incoming /help command.
+    """
     await message.answer(text=LEXICON['/help'],
                          reply_markup=help_command_button())
 
 
 @router.callback_query(F.data == 'home page')
 async def process_home_page_button(callback: CallbackQuery):
+    """
+    Handles the 'To home page' inline button click and sends a message with user options.
+
+    Args:
+        callback (CallbackQuery): The callback query object from aiogram representing the
+        incoming callback query when the 'To home page' button is clicked
+    """
     await callback.message.answer(text=LEXICON['/start'],
                                   reply_markup=user_choice_buttons().as_markup())
     await callback.answer()
