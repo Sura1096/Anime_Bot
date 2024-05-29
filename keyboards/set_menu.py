c33d7a48874e7cf8ed72ed893c9889fb82d5901a
Sura <suri.murodova@gmail.com>
2024-05-29 11:26:25 +0300

    Add type hints and docstrings

--- keyboards/set_menu.py
+++ keyboards/set_menu.py
@@ -3,7 +3,16 @@ from aiogram.types import BotCommand
 from lexicon.lexicon_menu_commands import LEXICON
 
 
-async def set_main_menu(bot: Bot):
+async def set_main_menu(bot: Bot) -> None:
+    """
+    Set the main menu commands for the bot using the provided lexicon.
+
+    This function takes a Bot instance and sets its main menu commands based
+    on the command-description pairs defined in the LEXICON dictionary.
+
+    Args:
+        bot (Bot): The instance of the bot for which to set the main menu commands.
+    """
     main_menu_commands = [
         BotCommand(
             command=command,
