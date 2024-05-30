2dfe225df4c4218186bfcf2d5c3340800c4d1021
Sura <suri.murodova@gmail.com>
2024-05-30 12:58:49 +0300

    Add docstring for SelectGenres class

--- states/states.py
+++ states/states.py
@@ -2,4 +2,10 @@ from aiogram.filters.state import State, StatesGroup
 
 
 class SelectGenres(StatesGroup):
+    """
+    State management for selecting genres in a bot.
+
+    Attributes:
+        selected_genres (State): The state representing the selection of genres by the user.
+    """
     selected_genres = State()
