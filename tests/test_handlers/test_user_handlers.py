955386b4411903aa6056c3d7c3aaa099c5272ab7
Sura <suri.murodova@gmail.com>
2024-06-04 10:15:14 +0300

    Add test for start command

---
+++ tests/test_handlers/test_user_handlers.py
@@ -0,0 +1,30 @@
+import pytest
+from unittest.mock import AsyncMock, patch
+from aiogram.types import Chat
+from types import SimpleNamespace
+
+from Git.Anime_Bot.handlers.user_handlers import (process_start_command,
+                                                  process_help_command,
+                                                  process_home_page_button)
+from Git.Anime_Bot.lexicon.lexicon import LEXICON
+from Git.Anime_Bot.keyboards.keyboard_utils import (user_choice_buttons,
+                                                    help_command_button)
+
+
+@pytest.mark.asyncio
+async def test_process_start_command():
+    message = SimpleNamespace(
+        message_id=1,
+        date=0,
+        chat=Chat(id=1, type='private'),
+        text='/start',
+        answer=AsyncMock()
+    )
+
+    with patch.object(message, 'answer', new=AsyncMock()) as mock_answer:
+        await process_start_command(message)
+
+        mock_answer.assert_awaited_once_with(
+            text=LEXICON['/start'],
+            reply_markup=user_choice_buttons().as_markup()
+        )
