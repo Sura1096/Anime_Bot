aeacbd6009dac3fdac495cabc21832458f656a3a
Sura <suri.murodova@gmail.com>
2024-06-13 10:34:15 +0300

    Add tests for genres method

---
+++ tests/test_external_services/test_API/test_genres.py
@@ -0,0 +1,38 @@
+import pytest
+
+from aioresponses import aioresponses
+
+from Git.Anime_Bot.external_services.API import JikanAPI
+
+
+anime = JikanAPI()
+url = f'https://api.jikan.moe/v4/genres/anime'
+
+
+@pytest.mark.asyncio
+async def test_genres():
+
+    with aioresponses() as mock:
+        mock_response = {
+            'data': [
+                {
+                    'mal_id': 1,
+                    'name': 'Action'
+                }
+            ]
+        }
+        mock.get(url, status=200, payload=mock_response)
+        response = await anime.genres()
+
+        assert 'data' in response
+        assert 'name' in response['data'][0]
+
+
+@pytest.mark.asyncio
+async def test_genres_not_found():
+
+    with aioresponses() as mock:
+        mock.get(url, status=404)
+        response = await anime.genres()
+
+        assert response == 'Not found'
