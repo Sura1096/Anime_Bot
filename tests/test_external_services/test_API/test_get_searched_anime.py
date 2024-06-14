4d2b156fa7d5d0009fd2f2854a689951f1bef187
Sura <suri.murodova@gmail.com>
2024-06-14 09:19:56 +0300

    Add tests for get_searched_anime method

---
+++ tests/test_external_services/test_API/test_get_searched_anime.py
@@ -0,0 +1,38 @@
+import pytest
+import urllib.parse
+
+from aioresponses import aioresponses
+
+from Git.Anime_Bot.external_services.API import JikanAPI
+
+
+anime = JikanAPI()
+url = f'https://api.jikan.moe/v4/anime'
+
+
+@pytest.mark.asyncio
+async def test_get_searched_anime():
+
+    with aioresponses() as mock:
+        mock_response = {
+            'pagination': {},
+            'data': []
+        }
+        parameters = {'q': 'Oshi no ko'}
+        encode_params = urllib.parse.urlencode(parameters)
+        mock.get(f"{url}?{encode_params}", status=200, payload=mock_response)
+        response = await anime.get_searched_anime(parameters)
+
+        assert 'data' in response
+
+
+@pytest.mark.asyncio
+async def test_get_searched_anime_not_found():
+
+    with aioresponses() as mock:
+        parameters = {'letter': '1, 2, 3'}
+        encode_params = urllib.parse.urlencode(parameters)
+        mock.get(f"{url}?{encode_params}", status=400)
+        response = await anime.get_searched_anime(parameters)
+
+        assert response == 'Not found'
