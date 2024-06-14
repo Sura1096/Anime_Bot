5dc9c62496b51d6e667acc3bb425982ef3042a92
Sura <suri.murodova@gmail.com>
2024-06-14 10:14:00 +0300

    Add tests for anime_type method

---
+++ tests/test_external_services/test_parse_data_from_api/test_anime_type.py
@@ -0,0 +1,27 @@
+import pytest
+
+from Git.Anime_Bot.external_services.parse_data_from_api import ParseAnimeData
+
+
+@pytest.mark.asyncio
+async def test_anime_type_with_type():
+    anime_data_with_type = {
+        'data': {
+            'type': 'TV'
+        }
+    }
+    parser = ParseAnimeData(anime_data_with_type)
+    anime_type = await parser.anime_type()
+    assert anime_type == '<b>Type:</b> TV'
+
+
+@pytest.mark.asyncio
+async def test_anime_type_no_type():
+    anime_data_no_type = {
+        'data': {
+            'type': None
+        }
+    }
+    parser = ParseAnimeData(anime_data_no_type)
+    anime_type = await parser.anime_type()
+    assert anime_type == '<i>No type information has been added to this title.</i>'
