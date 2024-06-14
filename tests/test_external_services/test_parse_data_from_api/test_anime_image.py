6ebb30ce6c127abeccdf52b318b0baf7de2697bf
Sura <suri.murodova@gmail.com>
2024-06-14 09:45:00 +0300

    Add tests for anime_image method

---
+++ tests/test_external_services/test_parse_data_from_api/test_anime_image.py
@@ -0,0 +1,37 @@
+import pytest
+
+from Git.Anime_Bot.external_services.parse_data_from_api import ParseAnimeData
+
+
+@pytest.mark.asyncio
+async def test_anime_image_with_image():
+    anime_data_with_image = {
+        'data': {
+            'images': {
+                'jpg': {
+                    'image_url': 'https://example.com/image.jpg'
+                }
+            }
+        }
+    }
+
+    parser = ParseAnimeData(anime_data_with_image)
+    image_url = await parser.anime_image()
+    assert image_url == 'https://example.com/image.jpg'
+
+
+@pytest.mark.asyncio
+async def test_anime_image_without_image():
+    anime_data_without_image = {
+        'data': {
+            'images': {
+                'jpg': {
+                    'image_url': None
+                }
+            }
+        }
+    }
+
+    parser = ParseAnimeData(anime_data_without_image)
+    image_url = await parser.anime_image()
+    assert image_url == '<i>Image has not been added to this title.</i>'
