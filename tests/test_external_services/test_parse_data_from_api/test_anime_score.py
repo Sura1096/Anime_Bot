a879d5acbff847dc9720832a56bf8f2b502b646d
Sura <suri.murodova@gmail.com>
2024-06-14 10:04:39 +0300

    Add tests for anime_score method

---
+++ tests/test_external_services/test_parse_data_from_api/test_anime_score.py
@@ -0,0 +1,27 @@
+import pytest
+
+from Git.Anime_Bot.external_services.parse_data_from_api import ParseAnimeData
+
+
+@pytest.mark.asyncio
+async def test_anime_score_with_score():
+    anime_data_with_score = {
+        'data': {
+            'score': 8.5
+        }
+    }
+    parser = ParseAnimeData(anime_data_with_score)
+    score = await parser.anime_score()
+    assert score == '<b>Score:</b> 8.5'
+
+
+@pytest.mark.asyncio
+async def test_anime_score_no_score():
+    anime_data_no_score = {
+        'data': {
+            'score': None
+        }
+    }
+    parser = ParseAnimeData(anime_data_no_score)
+    score = await parser.anime_score()
+    assert score == '<i>No score information has been added to this title.</i>'
