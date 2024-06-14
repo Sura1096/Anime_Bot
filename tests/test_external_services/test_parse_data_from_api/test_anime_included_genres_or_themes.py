bb403f539a8e5ed008fc72f2278d2830042e7bed
Sura <suri.murodova@gmail.com>
2024-06-14 10:09:52 +0300

    Add tests for anime_included_genres_or_themes method

---
+++ tests/test_external_services/test_parse_data_from_api/test_anime_included_genres_or_themes.py
@@ -0,0 +1,42 @@
+import pytest
+
+from Git.Anime_Bot.external_services.parse_data_from_api import ParseAnimeData
+
+
+@pytest.mark.asyncio
+async def test_anime_included_genres_or_themes_with_genres():
+    anime_data_with_genres = {
+        'data': {
+            'genres': [{'name': 'Action'}, {'name': 'Adventure'}],
+            'themes': None
+        }
+    }
+    parser = ParseAnimeData(anime_data_with_genres)
+    genres_or_themes = await parser.anime_included_genres_or_themes()
+    assert genres_or_themes == '<b>Genres:</b> Action, Adventure'
+
+
+@pytest.mark.asyncio
+async def test_anime_included_genres_or_themes_with_themes():
+    anime_data_with_themes = {
+        'data': {
+            'genres': None,
+            'themes': [{'name': 'Super Power'}, {'name': 'School'}]
+        }
+    }
+    parser = ParseAnimeData(anime_data_with_themes)
+    genres_or_themes = await parser.anime_included_genres_or_themes()
+    assert genres_or_themes == '<b>Themes:</b> Super Power, School'
+
+
+@pytest.mark.asyncio
+async def test_anime_included_genres_or_themes_no_genres_or_themes():
+    anime_data_no_genres_or_themes = {
+        'data': {
+            'genres': None,
+            'themes': None
+        }
+    }
+    parser = ParseAnimeData(anime_data_no_genres_or_themes)
+    genres_or_themes = await parser.anime_included_genres_or_themes()
+    assert genres_or_themes == '<i>No genres/themes information has been added to this title.</i>'
