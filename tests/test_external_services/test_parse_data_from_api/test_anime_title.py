5bfcaf55782b8080718d1961b14675fb16f7e85f
Sura <suri.murodova@gmail.com>
2024-06-14 10:50:15 +0300

    Add tests for ParseAnimeDataFromSearch method

--- tests/test_external_services/test_parse_data_from_api/test_anime_title.py
+++ tests/test_external_services/test_parse_data_from_api/test_anime_title.py
@@ -1,8 +1,9 @@
 import pytest
 
-from Git.Anime_Bot.external_services.parse_data_from_api import ParseAnimeData
+from Git.Anime_Bot.external_services.parse_data_from_api import ParseAnimeData, ParseAnimeDataFromSearch
 
 
+# Tests for ParseAnimeData class
 @pytest.mark.asyncio
 async def test_anime_title_both_titles():
     anime_data_with_both_titles = {
@@ -43,3 +44,45 @@ async def test_anime_title_only_original():
     parser = ParseAnimeData(anime_data_with_original_title)
     titles = await parser.anime_title()
     assert titles == '<b>Titles:</b> Boku no Hero Academia'
+
+
+# Tests for ParseAnimeDataFromSearch class
+@pytest.mark.asyncio
+async def test_anime_title_with_titles():
+    anime_data_with_titles = {
+        'data': [{
+            'title_english': 'My Hero Academia',
+            'title': 'Boku no Hero Academia'
+        }]
+    }
+    parser = ParseAnimeDataFromSearch(anime_data_with_titles)
+    titles = await parser.anime_title()
+    assert titles == 'My Hero Academia / Boku no Hero Academia'
+
+
+@pytest.mark.asyncio
+async def test_anime_title_english():
+    anime_data_with_english_title = {
+        'data': [{
+            'title_english': 'My Hero Academia',
+            'title': None
+        }]
+    }
+
+    parser = ParseAnimeDataFromSearch(anime_data_with_english_title)
+    titles = await parser.anime_title()
+    assert titles == 'My Hero Academia'
+
+
+@pytest.mark.asyncio
+async def test_anime_title_original():
+    anime_data_with_original_title = {
+        'data': [{
+            'title_english': None,
+            'title': 'Boku no Hero Academia'
+        }]
+    }
+
+    parser = ParseAnimeDataFromSearch(anime_data_with_original_title)
+    titles = await parser.anime_title()
+    assert titles == 'Boku no Hero Academia'
