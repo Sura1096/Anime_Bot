e60469972cee63054804409650f85c9d6869c1a8
Sura <suri.murodova@gmail.com>
2024-06-17 08:56:47 +0300

    Add tests for ParseAnimeDataFromSearch method

--- tests/test_external_services/test_parse_data_from_api/test_anime_status.py
+++ tests/test_external_services/test_parse_data_from_api/test_anime_status.py
@@ -1,8 +1,9 @@
 import pytest
 
-from Git.Anime_Bot.external_services.parse_data_from_api import ParseAnimeData
+from Git.Anime_Bot.external_services.parse_data_from_api import ParseAnimeData, ParseAnimeDataFromSearch
 
 
+# Tests for ParseAnimeData class
 @pytest.mark.asyncio
 async def test_anime_status_with_status():
     anime_data_with_status = {
@@ -25,3 +26,28 @@ async def test_anime_status_no_status():
     parser = ParseAnimeData(anime_data_no_status)
     status = await parser.anime_status()
     assert status == '<i>No status information has been added to this title.</i>'
+
+
+# Tests for ParseAnimeDataFromSearch class
+@pytest.mark.asyncio
+async def test_anime_status():
+    anime_data_with_status = {
+        'data': [{
+            'status': 'Finished Airing'
+        }]
+    }
+    parser = ParseAnimeDataFromSearch(anime_data_with_status)
+    status = await parser.anime_status()
+    assert status == 'Finished Airing'
+
+
+@pytest.mark.asyncio
+async def test_anime_status_without_status():
+    anime_data_without_status = {
+        'data': [{
+            'status': None
+        }]
+    }
+    parser = ParseAnimeDataFromSearch(anime_data_without_status)
+    status = await parser.anime_status()
+    assert status == "No status information has been added to this title."
