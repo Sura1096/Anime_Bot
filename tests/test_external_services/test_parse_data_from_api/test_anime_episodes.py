1dd600d74624c847b99a0b9de85279d6fd200f4d
Sura <suri.murodova@gmail.com>
2024-06-17 08:55:41 +0300

    Add tests for ParseAnimeDataFromSearch method

--- tests/test_external_services/test_parse_data_from_api/test_anime_episodes.py
+++ tests/test_external_services/test_parse_data_from_api/test_anime_episodes.py
@@ -1,8 +1,9 @@
 import pytest
 
-from Git.Anime_Bot.external_services.parse_data_from_api import ParseAnimeData
+from Git.Anime_Bot.external_services.parse_data_from_api import ParseAnimeData, ParseAnimeDataFromSearch
 
 
+# Tests for ParseAnimeData class
 @pytest.mark.asyncio
 async def test_anime_episodes_with_episodes():
     anime_data_with_episodes = {
@@ -25,3 +26,28 @@ async def test_anime_episodes_no_episodes():
     parser = ParseAnimeData(anime_data_no_episodes)
     episodes = await parser.anime_episodes()
     assert episodes == '<i>No episodes information has been added to this title.</i>'
+
+
+# Tests for ParseAnimeDataFromSearch class
+@pytest.mark.asyncio
+async def test_anime_episodes():
+    anime_data_with_episodes = {
+        'data': [{
+            'episodes': 24
+        }]
+    }
+    parser = ParseAnimeDataFromSearch(anime_data_with_episodes)
+    episodes = await parser.anime_episodes()
+    assert episodes == 24
+
+
+@pytest.mark.asyncio
+async def test_anime_episodes_without_episodes():
+    anime_data_without_episodes = {
+        'data': [{
+            'episodes': None
+        }]
+    }
+    parser = ParseAnimeDataFromSearch(anime_data_without_episodes)
+    episodes = await parser.anime_episodes()
+    assert episodes == "No episodes information has been added to this title."
