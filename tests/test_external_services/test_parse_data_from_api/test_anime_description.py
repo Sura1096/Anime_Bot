1da653078f85af0bc35e77cf56590b23683fd64e
Sura <suri.murodova@gmail.com>
2024-06-17 08:52:24 +0300

    Add tests for ParseAnimeDataFromSearch method

--- tests/test_external_services/test_parse_data_from_api/test_anime_description.py
+++ tests/test_external_services/test_parse_data_from_api/test_anime_description.py
@@ -1,8 +1,9 @@
 import pytest
 
-from Git.Anime_Bot.external_services.parse_data_from_api import ParseAnimeData
+from Git.Anime_Bot.external_services.parse_data_from_api import ParseAnimeData, ParseAnimeDataFromSearch
 
 
+# Tests for ParseAnimeData class
 @pytest.mark.asyncio
 async def test_anime_description_with_synopsis():
     anime_data_with_synopsis = {
@@ -25,3 +26,28 @@ async def test_anime_description_no_synopsis():
     parser = ParseAnimeData(anime_data_no_synopsis)
     description = await parser.anime_description()
     assert description == '<i>No description information has been added to this title.</i>'
+
+
+# Tests for ParseAnimeDataFromSearch class
+@pytest.mark.asyncio
+async def test_anime_description():
+    anime_data_with_synopsis = {
+        'data': [{
+            'synopsis': 'A great story about heroes.'
+        }]
+    }
+    parser = ParseAnimeDataFromSearch(anime_data_with_synopsis)
+    description = await parser.anime_description()
+    assert description == '<b>Description:</b> A great story about heroes.'
+
+
+@pytest.mark.asyncio
+async def test_anime_description_without_synopsis():
+    anime_data_without_synopsis = {
+        'data': [{
+            'synopsis': None
+        }]
+    }
+    parser = ParseAnimeDataFromSearch(anime_data_without_synopsis)
+    description = await parser.anime_description()
+    assert description == "No description information has been added to this title."
